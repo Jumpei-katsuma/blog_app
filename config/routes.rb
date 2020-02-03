9ac38ebceaea017b6034da9198a47b4c5bb3890d
Jumpei Katsuma <jumpeikatsuma@Glitchs-iMac.local>
2020-02-02 20:28:26 -0500

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  root to: "posts#index"
   devise_for :users
-  resources :posts
+  resources :posts, only: [:index, :show]
+
+  namespace :login do
+    resources :posts
+
+    namespace :admin do
+    resources :posts
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
